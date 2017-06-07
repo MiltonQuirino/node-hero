a0a296f8e1a6c0ae98e90f05791e3d0c1d7c1251
Milton Quirino <miltonquirino.br@gmail.com>
2017-06-07 16:26:42 -0300

    add hbs

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
 
-const app = express()
+const app = express();
 
-app.use((request, response, next) => {  
-  console.log(request.headers);
-  next();
-})
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, './app/views/layouts'),
+}));
 
-app.get('/', (request, response) => {  
-  response.json({
-    chance: request.chance,
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, './app/views'));
+
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'John',
   });
+});
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err);
+  response.status(500).send('Something broke!');
 });
 
 app.listen(3000);
